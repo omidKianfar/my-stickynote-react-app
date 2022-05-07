483a10e8c10811859fecc999e61eb551f2b4d164
omid kianfar <kianfar.omid.990@gmail.com>
2022-05-07 04:28:59 -0700

    Note Element

---
+++ src/Components/Note.js
@@ -0,0 +1,13 @@
+import React, { Children } from "react";
+
+const Note = () => {
+  return (
+    <div>
+      <p>Note</p>
+      <button>Edit</button>
+      <button>Delete</button>
+    </div>
+  );
+};
+
+export default Note;
